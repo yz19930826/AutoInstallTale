7ff39db5dec145bc862462028d90a757d970e092
HelloJava <yanzhansisi@gmail.com>
2018-03-25 13:22:32 +0800

    获取本机IP地址，输出有颜色的字体

--- Test.py
+++ Test.py
@@ -1,5 +1,8 @@
+# coding:utf-8
 # 测试文件读写
 import os
+
+
 
 # 将指定的内容写入文件
 def editFileContent(filepath,content):
@@ -27,6 +30,14 @@ def isContentInFile(filepath,content):
             return True
 
 
+def get_ip():
+    #注意外围使用双引号而非单引号,并且假设默认是第一个网卡,特殊环境请适当修改代码
+    out = os.popen("ifconfig | grep 'inet addr:' | grep -v '127.0.0.1' | cut -d: -f2 | awk '{print $1}' | head -1").read()
+    print out
+
 
 if __name__ == '__main__':
-    execShell('cd ..')
+    get_ip()
+    print("\033[1;34;47m恭喜您，搭建完成，访问:！\033[0m")
+    print("\033[1;34;47m想了解更多，请访问：www.hellojava.club\033[0m")
+
